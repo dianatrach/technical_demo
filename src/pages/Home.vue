e2fc6d638b595bd57cf24ab69fcf3ca6c09ba38d
Arthur <truchdiana@gmail.com>
2022-03-25 22:47:48 +0200

    Add button to load gifs, show gifs inside the cards

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,31 +1,56 @@
 <template>
     <div class="home">
         <div class="centered">
-            <div class="card" v-for="card in cards" :key="card.id">
-                <img src="https://giphy.com/gifs/hallmarkecards-cute-hallmark-shoebox-BzyTuYCmvSORqs1ABM">
-                <h1>{{card.id}}</h1>
+            <button class="button" @click=getGifs()>Search</button>
+            <div class="card" v-for="gif in gifs" :key="gif.id">
+                <img  :src="gif" >
             </div>        
         </div>
     </div>
 </template>
 
 <script>
-const apiKey ="wMqvSK3gHL65KRyFxTxyrNCUCJbskKtb"
+const apiKey ="wMqvSK3gHL65KRyFxTxyrNCUCJbskKtb";
+const limit = 20;
 export default ({
+    name: 'Home',
     data(){
         return {
-            cards:[]
+            cards:[],
+            gifs: []
         };
 
     },
     async mounted() {
-        const res =await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=cat&limit=3&offset=0&rating=g&lang=en`)
-        const cards = await res.json();
-        this.cards = cards.data;
-        console.log(cards);
-    }
+        // const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=cat&limit=3&offset=0&rating=g&lang=en`)
+        // const cards = await res.json();
+        // this.cards = cards.data;
+        // console.log(cards);
     
-})
+    },
+  methods: {
+    getGifs() {
+    
+      fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=cat&limit=${limit}&offset=0&rating=g&lang=en`)
+        .then(response => {
+          return response.json();
+        })
+        .then(json => {
+          this.buildGifs(json);
+        })
+        .catch(err => console.log(err));
+    
+    },
+    buildGifs(json) {
+      this.gifs = json.data.map(gif => gif.id).map(gifId => {
+        return `https://media.giphy.com/media/${gifId}/giphy.gif`;
+      });
+    }
+  }
+
+  
+    
+});
 </script>
 
 
